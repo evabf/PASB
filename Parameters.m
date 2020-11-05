41e84355dfb5fbffae607c678eda070ce648639e
evabf <73707902+evabf@users.noreply.github.com>
2020-11-05 15:12:14 +0100

    Update Parameters.m

--- Parameters.m
+++ Parameters.m
@@ -1,14 +1,15 @@
-parameters 
+Rat parameters 
 %Cardiac blood output [L/hr/kg^.75]
 QCC = 18.7; 
 
 %Fractional organ_i blood flow [L/hr/kg^.75]
-FQliver
-FQlung
-FQkidney 
-FQfat 
-FQgonads
-FQprostrate
+FQliver = 0.174;
+FQlung = 0.021;
+FQkidney = 0.141;
+FQbrain = 0.02;
+FQfat = 0.07;
+FQgonads = 0.0079;
+FQprostrate = 
 FQlung
 %Gastric empty
 GE %min^-1
