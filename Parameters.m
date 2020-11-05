33af5158fef588dd08f7532039c898a6387858c5
margh <margh@DESKTOP-TSOH1QD>
2020-11-05 14:55:20 +0100

    start

--- Parameters.m
+++ Parameters.m
@@ -1,2 +1,24 @@
 parameters 
+%Cardiac blood output [L/hr/kg^.75]
+QCC = 18.7; 
 
+%Fractional organ_i blood flow [L/hr/kg^.75]
+FQliver
+FQlung
+FQkidney 
+FQfat 
+FQgonads
+FQprostrate
+FQlung
+%Gastric empty
+GE %min^-1
+
+%constant fraction of prgans volume to body weight 
+Fliver
+Flung
+Fkidney
+Fgonads
+Fplasma
+Fprostrate
+HCT
+MSPL
