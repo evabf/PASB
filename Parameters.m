c7891ff83709c7faec4c4dfd6111802c6ebf8ffc
evabf <eva.bouffard@epfl.ch>
2020-11-11 11:52:12 +0100

    Update Parameters.m
    filled with human parameters, (push trial 1)

--- Parameters.m
+++ Parameters.m
@@ -1,2 +1,32 @@
 parameters 
 
+parameters 
+%Cardiac blood output [L/hr/kg^.75]
+QCC = 4.8; 
+
+%Fractional organ_i blood flow [L/hr/kg^.75]
+FQliver = 0.25;
+FQlung = 0.034;
+FQkidney = 0.177;
+FQfat = 0.052;
+FQgonads = 0.0012;
+FQprostrate = 0.0119;%newest value
+
+%Gastric empty
+GE = 3.5; %h^-1
+
+%constant fraction of prgans volume to body weight 
+Fliver = 0.026;
+Flung = 0.0104;
+Fkidney = 0.004;
+Ffat = 0.187;
+Fgonads = 0.0027;
+Fplasma = 0.0428;
+Fprostrate = 0.00068;%newest value
+HCT = 0.45;
+MSPL = 32; %mg/L liver %newest value(Barter et al., 2007)
+MSPG = 20.6; %mg/g gut
+CYTPL = 80.7; % mg/g liver
+CYTPG = 18; % mg/g gut
+MSPK = 16; %mg/g kidney
+CYTPK = 86; % mg/g kidney
