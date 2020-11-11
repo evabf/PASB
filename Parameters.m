99878ef4f44f8f1af6b090d94c1d2dd56fb60e22
evabf <73707902+evabf@users.noreply.github.com>
2020-11-11 10:59:28 +0100

    filled with human parameters

--- Parameters.m
+++ Parameters.m
@@ -1,24 +1,26 @@
 parameters 
 %Cardiac blood output [L/hr/kg^.75]
-QCC = 18.7; 
+QCC = 4.8; 
 
 %Fractional organ_i blood flow [L/hr/kg^.75]
-FQliver
-FQlung
-FQkidney 
-FQfat 
-FQgonads
-FQprostrate
-FQlung
+FQliver = 0.25;
+FQlung = 0.034;
+FQkidney = 0.177;
+FQfat = 0.052;
+FQgonads = 0.0012;
+FQprostrate = 0.0119;
+
 %Gastric empty
-GE %min^-1
+GE = 3.5; %min^-1
 
 %constant fraction of prgans volume to body weight 
-Fliver
-Flung
-Fkidney
-Fgonads
-Fplasma
-Fprostrate
-HCT
-MSPL
+Fliver = 0.026;
+Flung = 0.0104;
+Fkidney = 0.004;
+Fgonads = 0.0027;
+Fplasma = 0.0428;
+Fprostrate = 0.00068;
+HCT = 0.45;
+MSPL = 52.5;%mg/g liver
+MSPG = 20.6; % mg/g gut
+CYTPL = 80.7; %mg/g liver
